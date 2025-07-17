38c1abd19fe9f4498335bfeb5cde736ab44a2790
JaysonEdenfield <137231019+JaysonEdenfield@users.noreply.github.com>
2025-07-17 01:06:46 -0400

    Update theme.lua
    done

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,3 +1,11 @@
 return {
-
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    style = "storm",
+  },
+  config = function()
+    vim.cmd[[colorscheme tokyonight]],
+  end,
 }
