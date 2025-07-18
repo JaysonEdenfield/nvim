740f7662b0aac09fa18ead21528f8935c9f81b54
JaysonEdenfield <137231019+JaysonEdenfield@users.noreply.github.com>
2025-07-17 20:46:21 -0400

    Update lazy.lua
    typo fixes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,22 +1,21 @@
-local lpath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Check if it exists, if not, clone it from stable
-if not vim.uv.fs_stat(lpath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
       "git", "clone",
       "--filter=blob:none",
       "--branch=stable",
-      "https:/github.com/folke/lazy.nvim.git",
-      lpath,
+      "https://github.com/folke/lazy.nvim.git",
+      lazypath
   })
 end
 -- And finally, modify run time path to match
-vim.opt.rtp:prepend(lpath)
-
+vim.opt.rtp:prepend(lazypath)                         
 -- Setup
 require("lazy").setup({
     spec = {
-      { import = "plugins" },
-    },    
-    checker = { enabled = true },
-})
+      { import = "plugins" }
+    },
+    checker = { enabled = true }
+  })
