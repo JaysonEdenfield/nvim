76524ece8afb5958219e691cc18ec54df6062778
JaysonEdenfield <137231019+JaysonEdenfield@users.noreply.github.com>
2025-07-17 21:23:28 -0400

    Update tree-sitter.lua
    finished todo

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -4,6 +4,18 @@ return {
   config = function()
     require("nvim-treesitter.config").setup({
         -- TODO
+        ensure_installed = { "c", "lua", "cpp" } -- Add over time
+        highlight = {
+          enabled = true,
+          disable = function(lang, buf)
+            local max_file_size = 128 * 1024 -- 128kb
+            local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_file_size then
+              return true
+            end
+          end,
+        additional_vim_regex_highlighting = false,
+        }
     })
   end,
 }
